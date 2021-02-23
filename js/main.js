0d2a39ba629afb2aedce7fc3bda8a80523e1a8a6
zvlzvl <76951647+zvlzvl@users.noreply.github.com>
2021-02-23 19:02:18 +0200

    4 metu balansas done

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ import { menesiai } from './data.js';
 
 function zero(income) {
     if (income < 1 || income === undefined) {
+        income = 0
         return 0;
     }
 return income
@@ -14,19 +15,26 @@ function zeroExpense(expense) {
     }
 return expense
 }
-
+function balance(exp, inc) {
+    if (typeof(exp) !== 'number'){
+        return 0 - inc
+    } else if (typeof(inc) !== 'number') {
+        return exp
+    }
+    return parseInt(exp) - parseInt(inc)
+}
 
 function table(months, acountData) {
     let HTML = '';
     for (let i = 0; i < acountData.length; i++) {
         const object = acountData[i];
-        const balance = parseInt(object.income) - parseInt(object.expense);
+        
         HTML += `<div class="table-row">
                     <div class="cell">${i+1}</div>
                     <div class="cell">${months[object.month-1]}</div>
                     <div class="cell">${zero(object.income)} Eur</div>
                     <div class="cell">${zeroExpense(object.expense)} Eur</div>
-                    <div class="cell">${balance} Eur</div>
+                    <div class="cell">${balance(object.income, object.expense)} Eur</div>
                 </div>`;
         
     }
