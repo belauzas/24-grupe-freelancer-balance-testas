295edb99aefeae87b70755315858b8b1a6dd668d
Karolis Jachimavičius <76951309+KarolisVJ@users.noreply.github.com>
2021-02-23 23:00:56 +0200

    visos islaidos ir pajamos surasytos i lentele

--- js/main.js
+++ js/main.js
@@ -3,11 +3,16 @@ import {account} from './data.js';
 const sortedMonths = account.sort((a, b) => (a.month > b.month) ? 1 : -1);
 const menesiai = ["Sausis", "Vasaris", "Kovas", "Balandis", "Gegužė", "Birželis", "Liepa", "Rugpjūtis", "Rugsėjis", "Spalis", "Lapkritis", "Gruodis"];
 
+
+
 for (let i = 0; i < sortedMonths.length; i++) {
     sortedMonths[i].month = menesiai[i];
 }
 
-let HTML='';
+let HTML = '';
+let income = 0;
+let expenses = 0;
+let balance = 0;
 const DOM = document.querySelector('.table-content');
 
 console.log(sortedMonths[1].hasOwnProperty('income'));
@@ -22,7 +27,13 @@ for (let i = 0; i < sortedMonths.length; i++) {
     <div class="cell">${sortedMonths[i].income}</div>
     <div class="cell">${sortedMonths[i].expense}</div>
     <div class="cell">${sortedMonths[i].income - sortedMonths[i].expense}</div>
-</div>`}
+</div>`
+
+    income += sortedMonths[i].income;
+    expenses += sortedMonths[i].expense;
+    balance += sortedMonths[i].income - sortedMonths[i].expense;
+
+}
 
     else if (!sortedMonths[i].hasOwnProperty('income') && sortedMonths[i].hasOwnProperty('expense')) {
 
@@ -32,7 +43,12 @@ for (let i = 0; i < sortedMonths.length; i++) {
         <div class="cell"> - </div>
         <div class="cell">${sortedMonths[i].expense} </div>
         <div class="cell">${-sortedMonths[i].expense}</div>
-    </div>`}
+    </div>`
+
+    expenses += sortedMonths[i].expense;
+    balance += -sortedMonths[i].expense;
+
+}
 
     else if (sortedMonths[i].hasOwnProperty('income') && !sortedMonths[i].hasOwnProperty('expense')) {
 
@@ -42,7 +58,12 @@ for (let i = 0; i < sortedMonths.length; i++) {
         <div class="cell">${sortedMonths[i].income}</div>
         <div class="cell"> - </div>
         <div class="cell">${sortedMonths[i].income}</div>
-    </div>`}
+    </div>`
+
+        income += sortedMonths[i].income;
+        balance += sortedMonths[i].income;
+
+}
 
 
     else if (!sortedMonths[i].hasOwnProperty('income') && !sortedMonths[i].hasOwnProperty('expense')) {
@@ -54,9 +75,24 @@ for (let i = 0; i < sortedMonths.length; i++) {
         <div class="cell"> - </div>
         <div class="cell">0</div>
     </div>`}
+
+    
+
     }
 
-
 DOM.innerHTML = HTML;
+
+const DOMincome = document.querySelector('.table-footer > .cell:nth-of-type(3)');
+DOMincome.innerText = `${income}.00`;
+
+const DOMexpenses = document.querySelector('.table-footer > .cell:nth-of-type(4)');
+DOMexpenses.innerText = `${expenses}.00`;
+
+const DOMbalance = document.querySelector('.table-footer > .cell:nth-of-type(5)');
+DOMbalance.innerText = `${balance}.00`;
+
+console.log(income, expenses, balance);
+
+const sortedIncome = sortedMonths.sort((a,b))
 
 console.log(sortedMonths);
