8ab09b9b3ad46cab9878700dff41ad6de92a3bf6
Vadim <vadim.mozeiko@gmail.com>
2021-02-23 17:05:38 +0200

    Men pavadinimai

---
+++ js/main.js
@@ -0,0 +1,5 @@
+import{account} from './data.js'
+
+const menesiai = ['Sausis', 'Vasaris', 'Kovas', 'Balandis', 'Gegužė', 'Birželis', 'Liepa', 'Rugpjūtis', 'Rugsėjis', 'Spalis', 'Lapkritis', 'Gruodis']
+
+const months = account.map((month) => month.month)
