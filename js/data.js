db9c444ed33038960d76f42d6d1944b953f6854c
Paulius Jarutis <76951786+PJarrr@users.noreply.github.com>
2021-02-24 09:24:44 +0200

    new variable months added, imports/exports stated

--- js/data.js
+++ js/data.js
@@ -12,3 +12,20 @@ const account = [
     { month: 8, income: 450, expense: 120 },
     { month: 12, expense: 200 }
 ];
+
+const months = [
+"Sausis",
+"Vasaris",
+"Kovas",
+"Balandis",
+"Gegužė",
+"Birželis",
+"Liepa",
+"Rugpjūts",
+"Rugsėjis",
+"Spalis",
+"Lapkritis",
+"Gruodis"
+]
+
+export {account, months}
---
+++ js/main.js
@@ -0,0 +1,2 @@
+import {account, months} from './data.js'
+
