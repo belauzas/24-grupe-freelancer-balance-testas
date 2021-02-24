6121152edc942ee9e4d91b46baef725e03c469b7
MarkBukowski <MarkBukowski@users.noreply.github.com>
2021-02-24 13:18:32 +0200

    5. footer table

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 import { account, months } from './data.js';
 
 
-function renderTable(selector) {
-    const DOM = document.querySelector(selector);
+function renderTable() {
+    const tableDOM = document.querySelector('.table-content');
+    const footerDOM = document.querySelector('.table-footer');
     let HTML = '';
+    let totalIncome = 0;
+    let totalExpense = 0;
     let total = 0;
 
     for (let i = 0; i < months.length; i++) {
@@ -16,22 +19,38 @@ function renderTable(selector) {
         if (!money.expense) {
             money.expense = 0;
         }
-
         total += money.income;
         total -= money.expense;
-
-
+        totalIncome += money.income;
+        totalExpense += money.expense;
+        if (!money.income) {
+            money.income = '-';
+        }
+        if (!money.expense) {
+            money.expense = '-';
+        }
         HTML += `
-    <div class="table-row">
-        <div class="cell">${month.index}</div>
-        <div class="cell">${month.monthName}</div>
-        <div class="cell">${money.income}</div>
-        <div class="cell">${money.expense}</div>
-        <div class="cell">${total}</div>
-    </div>`
-
+            <div class="table-row">
+                <div class="cell">${month.index}</div>
+                <div class="cell">${month.monthName}</div>
+                <div class="cell">${money.income}</div>
+                <div class="cell">${money.expense}</div>
+                <div class="cell">${total}</div>
+            </div>`
     }
-    DOM.innerHTML = HTML;
+    tableDOM.innerHTML = HTML;
+
+    HTML = `
+                <div class="cell"></div>
+                <div class="cell"></div>
+                <div class="cell">${totalIncome} Eur</div>
+                <div class="cell">${totalExpense} Eur</div>
+                <div class="cell total">${total} Eur</div>
+            `
+    footerDOM.innerHTML = HTML;
 }
 
-renderTable('.table-content');
+renderTable();
+
+
+
